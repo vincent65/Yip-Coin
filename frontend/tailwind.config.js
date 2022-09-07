9a2e4d5b0744fa2f2fb80eea0024e4f5d84f4f88
Vincent Yip <vincentyip@Vincents-MBP.fios-router.home>
2022-09-07 14:48:38 -0400

    frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsxx}",
+    "./components/**/*.{js,ts,jsx,tsxx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
